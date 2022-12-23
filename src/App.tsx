a237499c84e8d90fb9cdd10c370a281da1e7d763
Dawid Olczak <dawid.a.olczak@gmail.com>
2022-12-23 18:25:23 +0100

    Fixed MainLayout

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import {MainLayout} from "./layouts/MainLayout";
-import {Grid, Paper} from "@mui/material";
+import {useAuth} from "./hooks/useAuth";
+import {AuthenticatedApp} from "./pages/AuthenticatedApp";
+import { UnAuthenticatedApp } from './pages/UnAuthenticatedApp';
 
+export const App = () => {
 
+    const {user} = useAuth()
 
-function App() {
   return (
     <>
-    <MainLayout>
-      <>
-        <Grid item xs={12} md={8} lg={9}>
-          <Paper
-              sx={{
-                p: 2,
-                display: 'flex',
-                flexDirection: 'column',
-                height: 240,
-              }}
-          >
-              <i className="fa-solid fa-cars"></i>
-          </Paper>
-        </Grid>
-
-        <Grid item xs={12} md={4} lg={3}>
-          <Paper
-              sx={{
-                p: 2,
-                display: 'flex',
-                flexDirection: 'column',
-                height: 240,
-              }}
-          >
-            <p>KARTA JAKAS TAM 2 </p>
-          </Paper>
-        </Grid>
-
-        <Grid item xs={12}>
-          <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
-            <p>KARTA JAKAS TAM 3</p>
-          </Paper>
-        </Grid>
-      </>
-    </MainLayout>
+        {user ? <AuthenticatedApp /> : <UnAuthenticatedApp />}
     </>
   );
 }
