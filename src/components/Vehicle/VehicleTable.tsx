64f77588bf399008e14b711165a1dcfa4b0ad531
Dawid Olczak <dawid.a.olczak@gmail.com>
2023-02-26 20:32:32 +0100

    fix

--- src/components/Vehicle/VehicleTable.tsx
+++ src/components/Vehicle/VehicleTable.tsx
@@ -8,53 +8,16 @@ import { useTheme } from '@mui/material/styles';
 import {GetListOfVehiclesResponse} from 'types'
 import {VehicleSingleItem} from "./VehicleSingleItem";
 import {
-    Avatar,
     Box,
     FormControl,
     IconButton,
     InputLabel,
-    MenuItem,
+    MenuItem, Pagination,
     Select,
     SelectChangeEvent,
     TablePagination,
-    TextField, Tooltip
+    TextField
 } from "@mui/material";
-import {toast} from "react-toastify";
-import onChange = toast.onChange;
-import {KeyboardArrowLeft, KeyboardArrowRight} from "@mui/icons-material";
-import LastPageIcon from '@mui/icons-material/LastPage';
-import FirstPageIcon from '@mui/icons-material/FirstPage';
-import {DataGrid, GridColDef, GridValueGetterParams} from "@mui/x-data-grid";
-import BusAlertIcon from "@mui/icons-material/BusAlert";
-import {Link} from "react-router-dom";
-import DeleteIcon from "@mui/icons-material/Delete";
-
-const columns: GridColDef[] = [
-    { field: 'photo', headerName: 'Zdjęcie', width: 150, renderCell: (params) => (
-            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }} src={params.value}/>
-        ) },
-    { field: 'registerNumber', headerName: 'Numer rejestracyjny', width: 160 },
-    { field: 'name', headerName: 'Marka', width: 120 },
-    { field: 'model',  headerName: 'Model',  width: 100},
-    { field: 'vehicleType',  headerName: 'Typ',  width: 100},
-    { field: 'place',  headerName: 'Oddział',  width: 100},
-    { field: 'settings',  headerName: 'Opcje',  width: 100,align: 'right' , renderCell: (params) => (
-            <>
-                <Tooltip title="Usuń pojazd">
-                    <IconButton>
-                        <DeleteIcon />
-                    </IconButton>
-                </Tooltip>
-                <Link to={`/vehicle/${params.id}`}>
-                    <Tooltip title="Profil pojazdu">
-                        <IconButton>
-                            <BusAlertIcon />
-                        </IconButton>
-                    </Tooltip>
-                </Link>
-            </>
-        )},
-];
 
 
 
@@ -64,25 +27,27 @@ export const VehicleTable = () => {
     const [order, setOrder] = useState<any>('asc');
     const [sort, setSort] = useState('')
     const [sortValue, setSortValue] = useState('')
-    const [resultsCount, setResultsCount] = useState(0)
     const [page, setPage] = useState(1);
     const [rowsPerPage, setRowsPerPage] = useState(2);
+    const [maxPage, setMaxPage] = useState(0)
 
     useEffect(() => {
         (async () => {
 
-            const res = await fetch(`http://localhost:3002/vehicle/list?page=${page}&order=${order}&${sort}=${sortValue}`, {
+            const res = await fetch(`http://localhost:3002/vehicle/list?page=${page}&count=${rowsPerPage}&order=${order}&${sort}=${sortValue}`, {
                 credentials: 'include',
             })
             const data = await res.json()
             console.log(data)
-            setResultsCount(data.resultsCount)
+            setMaxPage(data.pagesCount)
             setVehiclesList(data.vehicles)
 
         })();
-    }, [page, sort, sortValue]);
+    }, [rowsPerPage, page, sort, sortValue]);
 
-
+    const handleChange = (e: any, p: any) => {
+        setPage(p)
+    }
 
     if (vehiclesList === null) {
         return <p>Wczytywanie...</p>
@@ -91,14 +56,29 @@ export const VehicleTable = () => {
 
     return(
         <>
-            <Box style={{ height: 450, width: `100%` }}>
-                <DataGrid
-                    rows={vehiclesList}
-                    columns={columns}
-                    pageSize={10}
-                    rowsPerPageOptions={[10]}
-                />
+            <Box>
+                <Pagination count={maxPage} onChange={handleChange} />
             </Box>
+            <Table size="small">
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Zdjecie</TableCell>
+                        <TableCell>Numer rejestracyjny</TableCell>
+                        <TableCell>Marka</TableCell>
+                        <TableCell>Model</TableCell>
+                        <TableCell>Typ</TableCell>
+                        <TableCell>Oddział</TableCell>
+                        <TableCell align="right"></TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {
+                        vehiclesList.map(vehicle => (
+                            <VehicleSingleItem vehicle={vehicle} key={vehicle.id}/>
+                        ))
+                    }
+                </TableBody>
+            </Table>
         </>
     )
 }
